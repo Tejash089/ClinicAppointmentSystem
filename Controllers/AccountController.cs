86bca8c04c20195df2eb576f9f0a2feace6f145a
Tejash089 <ganvittejash20@gmail.com>
2025-09-25 22:19:32 +0530

    Update AccountController.cs

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -66,7 +66,7 @@ namespace ClinicAppointmentSystem.Controllers
             return View(model);
         }
 
-        // GET: Login
+        // GET: Login 
         [HttpGet]
         public IActionResult Login() => View();
 
